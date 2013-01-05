5f3751134b061fe3e1cc7bd52abc2d7d597161e4
Hiroki Yoshioka <irohiroki@gmail.com>
2013-01-05 14:23:12 +0900

    add before and after BlockToDoEnd files

---
+++ after-BlockToDoEnd.rb
@@ -0,0 +1,52 @@
+f do
+end
+f do |a|
+end
+f do |a|
+end
+f do
+end
+f do
+  a
+end
+f do
+  a
+end
+f do |a|
+  a
+end
+f do |a|
+  a
+end
+f do |a|
+  a
+end
+f do |a|
+  a
+end
+f do
+end
+f do |a|
+end
+f do |a|
+end
+f do
+end
+f do
+  a
+end
+f do
+  a
+end
+f do |a|
+  a
+end
+f do |a|
+  a
+end
+f do |a|
+  a
+end
+f do |a|
+  a
+end
